8a3519cf4fe16bb5d5ad6dd6f79bf18ff14b7d66
Rajat Dixit <Rajatdixithbtu@gmail.com>
2023-06-09 23:16:57 +0530

    add new

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,28 +5,25 @@ import java.sql.SQLOutput;
 public class Main {
 
     public static void main(String[] args) {
-        Product p;
-        p = new Product();
-        System.out.println(p.product(5, 6));
-        System.out.println(p.product(5, 6, 7));
-        System.out.println(p.product(2.3, 4.6));
+        Product p = new Product();
+        p.product(5, 6);
+     p.product(5, 6, 7);
+       p.product(2.3, 4.6);
     }
+
+    public static   class  Product {
+        public int product(int x, int y) {
+            return x + y;
+        }
+
+
+        public int product(int x, int y, int z) {
+            return x + y + z;
+        }
+
+        public double product(double x, double y) {
+            return x + y;
+        }
+    }
+
 }
-        class Product {
-        public int product(int x, int y)
-        {
-            return x+y;
-        }
-
-
-
-        public int product(int x, int y, int z)
-        {
-            return x+y+z;
-        }
-        public double product(double x, double y)
-        {
-            return x+y;
-        }
-    }
-
